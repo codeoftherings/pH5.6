416ace182a2de9cd6015e5cadb0e0957aa222567
Sagnik Bhattacharya <saggieb12@gmail.com>
2015-12-11 22:09:09 +0530

    Added Arduino code to control servo motors

---
+++ servo.ino
@@ -0,0 +1,30 @@
+#include<Servo.h>
+
+int servoPin=9;
+int inputPin=8;
+int angle=0;
+Servo servo;
+void setup() {
+ servo.attach(servoPin);
+ pinMode(inputPin, INPUT);
+}
+
+void loop() {
+  if(digitalRead(inputPin)==HIGH)
+  {
+    for(angle=0;angle<=180;angle++)
+    {
+      servo.write(angle);
+    }
+    
+  }
+    else
+   {
+    for(angle=180;angle>=0;angle--)
+    {
+      servo.write(angle);
+    }
+    
+  }
+
+}
