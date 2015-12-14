b54d1a370239fe88ce276068d33de676dcd06ab5
Anisha Bhattacharya <Anisha Bhattacharya>
2015-12-15 00:54:36 +0530

    improvements

--- app.py
+++ app.py
@@ -12,13 +12,13 @@ GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(echo,GPIO.IN)
 GPIO.setup(28, GPIO.OUT)
 GPIO.output(28, False)
-#toAdd=str(raw_input('add email'))
+toAdd=str(raw_input('add email'))
 toAdd="saggieb12@gmail.com"
-#os.system('modprobe w1-gpio')
-#os.system('modprobe w1-therm')
-#base_dir = '/sys/bus/w1/devices/'
-#device_folder = glob.glob(base_dir + '28*')[0]
-#device_file = device_folder + '/w1_slave'
+os.system('modprobe w1-gpio')
+os.system('modprobe w1-therm')
+base_dir = '/sys/bus/w1/devices/'
+device_folder = glob.glob(base_dir + '28*')[0]
+device_file = device_folder + '/w1_slave'
 
   
 @app.route("/")
@@ -106,31 +106,31 @@ def ultrasoundDisplay():
    fill=10-cm
    fillpercent=(fill/10)*100
    fillpercent=round(fillpercent, 2)
-  # if cm<=10.0:
+   if cm<=10.0:
 
         
-        # sendEmail()
+         sendEmail()
    
 
    return render_template('us.html', ultrasound=fillpercent)
 
 
-#def read_temp_raw():
-#    f = open(device_file, 'r')
-#    lines = f.readlines()
-#    f.close()
-#    return lines
+def read_temp_raw():
+    f = open(device_file, 'r')
+    lines = f.readlines()
+    f.close()
+    return lines
  
-#def read_temp():
-#    lines = read_temp_raw()
-#    while lines[0].strip()[-3:] != 'YES':
-#        time.sleep(0.2)
-#        lines = read_temp_raw()
-#    equals_pos = lines[1].find('t=')
-  #  if equals_pos != -1:
- #       temp_string = lines[1][equals_pos+2:]
-#       temp_c = float(temp_string) / 1000.0
-#        return temp_c
+def read_temp():
+    lines = read_temp_raw()
+    while lines[0].strip()[-3:] != 'YES':
+        time.sleep(0.2)
+        lines = read_temp_raw()
+    equals_pos = lines[1].find('t=')
+    if equals_pos != -1:
+        temp_string = lines[1][equals_pos+2:]
+        temp_c = float(temp_string) / 1000.0
+        return temp_c
 
 if __name__=='__main__':
         app.run(host='0.0.0.0', port=80,debug=True)
