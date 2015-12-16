0b55b278c7930d11bf8e656adf59ed86ce5f619b
Sagnik Bhattacharya <saggieb12@gmail.com>
2015-12-16 13:37:59 +0530

    Commented the code

--- app.py
+++ app.py
@@ -1,6 +1,8 @@
+#code that will run the Raspberry Pi
 import RPi.GPIO as GPIO
 import time
 import os, glob, time
+#importing the Flask library to run webapp
 from flask import Flask, render_template
 import smtplib
 app=Flask(__name__)
@@ -9,31 +11,35 @@ ldrpin=21
 trig=23
 echo=24
 linkitLink=15
+#initializing all the GPIO elements
 GPIO.setup(trig, GPIO.OUT)
 GPIO.setup(echo,GPIO.IN)
 GPIO.setup(linkitLink, GPIO.OUT)
+#Initializing pin that will communicate with the LinkIt
 GPIO.output(linkitLink, False)
+#taking inut for email
 toAdd=str(raw_input('add email'))
+#Initializing system to read  temperature
 os.system('modprobe w1-gpio')
 os.system('modprobe w1-therm')
 base_dir = '/sys/bus/w1/devices/'
 device_folder = glob.glob(base_dir + '28*')[0]
 device_file = device_folder + '/w1_slave'
 
-  
+#Start Page  
 @app.route("/")
 def setup1():
     return render_template('start.html')
 
 
-
+#Page that will render temperature reading
 @app.route("/temp")
 def temperature():
     return render_template('temp.html', tempRead=read_temp())
     
 
 
-
+#SMTP process to send email
 def sendEmail():
     username="saggieb12@gmail.com"
     password="149162536" 
@@ -54,13 +60,13 @@ def sendEmail():
 
     
 
-
+#home page that will provide all the data together
 @app.route("/home")
 def home():
     return render_template('home.html')
     GPIO.output(linkitLink, False)
     
-
+#provide data for clarity of water
 @app.route("/ldr")
 def ldrDisplay():
     reading=0
@@ -77,13 +83,13 @@ def ldrDisplay():
     print (contamination)
     time.sleep(1)
     return render_template('ldr.html', ldr=contamination)
-
+#Page that will render when lid is open
 @app.route("/openLid")
 def openLid():
-    GPIO.output(linkitLink, True)
+    GPIO.output(linkitLink, True) #sending digital ignla to the LinkIt
     return render_template('home_lid_open.html');
     
-    
+#Calculating data for water level
 @app.route("/ultrasound")
 def ultrasoundDisplay():
    GPIO.output(trig,False)
@@ -114,13 +120,14 @@ def ultrasoundDisplay():
 
    return render_template('us.html', ultrasound=fillpercent)
 
-
+#Opening the file to read the raw data
 def read_temp_raw():
     f = open(device_file, 'r')
     lines = f.readlines()
     f.close()
     return lines
- 
+    
+#Streamlining the raw data to extract the temperature reading
 def read_temp():
     lines = read_temp_raw()
     while lines[0].strip()[-3:] != 'YES':
@@ -131,6 +138,6 @@ def read_temp():
         temp_string = lines[1][equals_pos+2:]
         temp_c = float(temp_string) / 1000.0
         return temp_c
-
+#running the Flask app
 if __name__=='__main__':
         app.run(host='0.0.0.0', port=80,debug=True)
