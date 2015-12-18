637d1424302ad2344fbbd5e2629629ea23f8382a
Sagnik Bhattacharya <saggieb12@gmail.com>
2015-12-18 23:31:56 +0530

    changed servo code

--- servo/servo.ino
+++ servo/servo.ino
@@ -1,32 +1,43 @@
 #include<Servo.h>
 
-int servoPin=9;
-int servoPin2=10;
+int servoPin=3;
+int servoPin2=9;
 int inputPin=8;
 int angle=0;
+bool isHigh=true;
+bool isLow=true;
 Servo servo;
+Servo servo2;
 void setup() {
  servo.attach(servoPin);
- servo.attach(servoPin2);
+ servo2.attach(servoPin2);
  pinMode(inputPin, INPUT);
 }
 
 void loop() {
-  if(digitalRead(inputPin)==HIGH)
+ if(digitalRead(inputPin)==HIGH && isHigh)
   {
-    for(angle=0;angle<=180;angle++)
+    for(angle=60;angle<180;angle++)
     {
       servo.write(angle);
+      servo2.write(angle);
+     
+      delay(15);
     }
-    
-  }
-    else
+    isHigh=false;
+    isLow=true;
+}
+    else if (digitalRead(inputPin)==LOW && isLow))
    {
-    for(angle=180;angle>=0;angle--)
-    {
-      servo.write(angle);
-    }
+   for(angle=180;angle>60;angle--)
+   {
+     servo.write(angle);
+     servo2.write(angle);
+     delay(15);
     
+    }
+    isHigh=true;
+    isLow=false;
   }
 
 }
